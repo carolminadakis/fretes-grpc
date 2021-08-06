4bc29bfaf5e35a734d85cbcb137e763e8b8a153c
Ana Carolina Minadakis <ana.minadakis@zup.com.br>
2021-08-06 11:05:49 -0300

    projeto fretes, aprendendo GRPC

---
+++ src/main/kotlin/br/com/zup/FretesGrpcServer.kt
@@ -0,0 +1,28 @@
+package br.com.zup
+
+import io.grpc.stub.StreamObserver
+import org.slf4j.LoggerFactory
+import javax.inject.Singleton
+import kotlin.random.Random
+
+@Singleton
+class FretesGrpcServer : FretesServiceGrpc.FretesServiceImplBase() {
+
+    private val logger = LoggerFactory.getLogger(FretesGrpcServer::class.java)
+
+    override fun calculaFrete(request: CalculaFreteRequest?, responseObserver: StreamObserver<CalculaFreteResponse>?) {
+
+        logger.info("Calculando frete para request: $request")
+
+        val response = CalculaFreteResponse.newBuilder()
+            .setCep(request!!.cep)
+            .setValor(Random.nextDouble(from = 0.0, until = 140.0))
+            .build()
+
+        logger.info("Frete calculado: $response")
+
+        responseObserver!!.onNext(response)
+        responseObserver.onCompleted()
+
+    }
+}
